3035005856d3f8c05d6c489807159772ab05ab77
valerio <valerio.deprosperi@gmail.com>
2020-08-30 13:26:41 +0200

    var bombe e tentartivi e  funct check e random..array numbombe

--- js/script.js
+++ js/script.js
@@ -5,24 +5,16 @@
 //Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
 //La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
 //Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
-​
 //var bombe = 16;
 //var tentativi = 100;
-​
 //funzione che genera un numero casuale tra 1 e 100
-​
 //funzione che dato un array e un numero mi controlla che il numero non sia già nell'array
-​
 //creo variabile array che contiene i numeri bomba
-​
 //ciclo che chiama la mia funzione di generazione dei numeri 16 volte  while(i<bombe)
     //check che il numero non sia duplicato
     //se non è duplicato, lo salvo nell'array bombe
-​
 //creo variabile array che contiene i numeri utente
-​
 //variabili booleana haPerso = false
-​
 //ciclo di 100-16 volte  //for(i=0; i<tentativi-bombe; i++)
   //prompt in cui chiedo all'utente un numero
   //check che il numero sia compreso tra 1 e 100
@@ -33,7 +25,25 @@
       //alert con array utente .length -> numero di volte che l'utente ha inserito un numero corretto
       //haPerso = true;
       //esco dal ciclo
-​
 //controllo se haPerso è false
   //alert hai vinto
   //alert con array utente .length -> numero di volte che l'utente ha inserito un numero corretto
+
+var bombe = 16;
+var tentativi = 100;
+
+function getRandomBetween(min, max) {
+    return Math.random() * (max - min) + min;
+    console.log(getRandomBetween);
+  }
+
+  function check(numero, array){
+
+  for (var i = 0; i < array.length; i++){
+    if (numero == array[i]) {
+      return true;
+    }
+  }
+   }
+
+   var numBombe = [];
